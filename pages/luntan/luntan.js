9de552caf539d6e321a84a8ffb64a6c9dab6bc54
Zoedusy <zoedusy123@gmail.com>
2021-11-16 19:56:53 +0800

    luntan

--- pages/luntan/luntan.js
+++ pages/luntan/luntan.js
@@ -1,66 +1,135 @@
-// pages/luntan/luntan.js
+// pages/mall/mall.js
+const app = getApp()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    detail:{},
+    goods:[
+      {
+        good_img:"https://img.xiaohongshu.com/items/c1a0056ba9ba17b30446a0e040dc9e30@800w_90Q_1x_2o.jpg",
+        title: "香氛有助于缓解心情",
+        desc:"1、栀子花香氛精油：能够改善睡眠缓解不安情绪，加强心肌收缩力对衰竭心脏作用更加显著。 2、葡萄柚精油：最具有制怒的作用，香氛可以进一步适当地缓解紧张度。",
+        // current_price:"￥89",
+        // original_price:"￥138",
+        // sign_img:"https://img.xiaohongshu.com/seller/0d1b063d887360fc5a3779bd6784a453"
+      },
+      {
+        good_img:"https://img.xiaohongshu.com/items/c1a0056ba9ba17b30446a0e040dc9e30@800w_90Q_1x_2o.jpg",
+        title: "香氛有助于缓解心情",
+        desc:"1、栀子花香氛精油：能够改善睡眠缓解不安情绪，加强心肌收缩力对衰竭心脏作用更加显著。 2、葡萄柚精油：最具有制怒的作用，香氛可以进一步适当地缓解紧张度。",
+        // current_price:"￥89",
+        // original_price:"￥138",
+        // sign_img:"https://img.xiaohongshu.com/seller/0d1b063d887360fc5a3779bd6784a453"
+      },
+      {
+        good_img:"https://img.xiaohongshu.com/items/c1a0056ba9ba17b30446a0e040dc9e30@800w_90Q_1x_2o.jpg",
+        title: "香氛有助于缓解心情",
+        desc:"1、栀子花香氛精油：能够改善睡眠缓解不安情绪，加强心肌收缩力对衰竭心脏作用更加显著。 2、葡萄柚精油：最具有制怒的作用，香氛可以进一步适当地缓解紧张度。",
+        // current_price:"￥89",
+        // original_price:"￥138",
+        // sign_img:"https://img.xiaohongshu.com/seller/0d1b063d887360fc5a3779bd6784a453"
+      },
+      {
+        good_img:"https://img.xiaohongshu.com/items/c1a0056ba9ba17b30446a0e040dc9e30@800w_90Q_1x_2o.jpg",
+        title: "香氛有助于缓解心情",
+        desc:"1、栀子花香氛精油：能够改善睡眠缓解不安情绪，加强心肌收缩力对衰竭心脏作用更加显著。 2、葡萄柚精油：最具有制怒的作用，香氛可以进一步适当地缓解紧张度。",
+        // current_price:"￥89",
+        // original_price:"￥138",
+        // sign_img:"https://img.xiaohongshu.com/seller/0d1b063d887360fc5a3779bd6784a453"
+      },
+      {
+        good_img:"https://img.xiaohongshu.com/items/c1a0056ba9ba17b30446a0e040dc9e30@800w_90Q_1x_2o.jpg",
+        title: "香氛有助于缓解心情",
+        desc:"1、栀子花香氛精油：能够改善睡眠缓解不安情绪，加强心肌收缩力对衰竭心脏作用更加显著。 2、葡萄柚精油：最具有制怒的作用，香氛可以进一步适当地缓解紧张度。",
+        // current_price:"￥89",
+        // original_price:"￥138",
+        // sign_img:"https://img.xiaohongshu.com/seller/0d1b063d887360fc5a3779bd6784a453"
+      },
+      {
+        good_img:"https://img.xiaohongshu.com/items/c1a0056ba9ba17b30446a0e040dc9e30@800w_90Q_1x_2o.jpg",
+        title: "香氛有助于缓解心情",
+        desc:"1、栀子花香氛精油：能够改善睡眠缓解不安情绪，加强心肌收缩力对衰竭心脏作用更加显著。 2、葡萄柚精油：最具有制怒的作用，香氛可以进一步适当地缓解紧张度。",
+        // current_price:"￥89",
+        // original_price:"￥138",
+        // sign_img:"https://img.xiaohongshu.com/seller/0d1b063d887360fc5a3779bd6784a453"
+      }
+    ]
+  },
+  
+  entrySearch(e) {
+    wx.navigateTo({
+      url: '../luntan/searchbar/searchbar',
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.setNavigationBarTitle({
+      title: '论坛',
+    })
+    wx.request({               //请求数据
+      // url: 'https://www.easy-mock.com/mock/5b1e17a0d4a14a3247a6cd6b/data',
+      success: (res) => {
+        this.setData({
+          goods: res.data.data
+        })
+        console.log(this.data.goods)
+      }
+    })  
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+  
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+  
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+  
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+  
   }
 })
---
+++ pages/luntan/searchbar/searchbar.js
@@ -0,0 +1,203 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    historyRecord:[{
+      id: '0',
+      recordItem:' '
+    }],
+    hots:[{
+      id:'01',
+      text:'头疼',
+      hotStatus:132
+    },{
+      id:'02',
+      text:'焦虑',
+      hotStatus:103
+    },{
+      id:'03',
+      text:'无法快乐',
+      hotStatus:43
+    },{
+      id:'04',
+      text:'安睡',
+      hotStatus:38
+    },{
+      id:'05',
+      text:'释放心情',
+      hotStatus:45
+    },{
+      id:'06',
+      text:'非处方类药物',
+      hotStatus:92
+    },{
+      id:'07',
+      hotImg:'/images/hot.png',
+      text:'香氛',
+      hotStatus:156
+    },{
+      id:'08',
+      text:'冥想',
+      hotStatus:85
+    }],
+    searchContext:'',
+    haveSerachLike: false,
+    searchLikeList: [],
+    searchLikeAllList: [{
+      text: '抑郁症如何释放心情'
+    }, {
+      text: '世界杯赛程'
+    }, {
+      text: '世界杯狂欢色'
+    }, {
+      text: '为世界杯干杯'
+    }, {
+      text: '世界杯球迷上线'
+    }, {
+      text: '世界杯没有时差'
+    },{
+      text:'...'
+    }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var hots = this.data.hots;
+    var hots2 = hots.sort((x, y) => y.hotStatus - x.hotStatus);
+    // reverse()方法会反转数组项的顺序
+    // hots.reverse();
+    console.log(hots2);
+    this.setData({
+      hots: hots2
+    })
+
+    wx.getStorage({
+      key: 'historyRecord',
+      success: (res) => {
+        // success
+        this.setData({
+          historyRecord: res.data
+        })
+      },
+      fail: function() {
+        // fail
+      },
+      complete: function() {
+        // complete
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  },
+  bindconfirm: function(e){
+    console.log(e);
+    var recordItem = e.detail.value;
+    this.saveHistory({
+      id: 0,
+      recordItem
+    })
+    wx.navigateTo({
+      url: '../searchbar/searchbar',
+    })
+    this.setData({
+      searchContext:''
+    })
+  },
+  changeSearch (e) {
+    let value = e.detail.value
+    if (value === '') {
+      this.setData({
+        haveSerachLike: false
+      })
+      return
+    }
+    let arr = this.data.searchLikeAllList.filter(item => item.text.indexOf(value) > -1)
+    console.log(arr)
+    this.setData({
+      haveSerachLike: true,
+      searchLikeList: arr,
+    })
+  },
+  backTo () {
+    wx.navigateBack({
+      delta: 1
+    })
+  },
+  deleteRecord: function(e){
+    console.log(e);
+    let filterArr = this.data.historyRecord.filter((item, index) => {
+      return index !== e.target.dataset.index
+    })
+
+    this.setData({
+      historyRecord: filterArr
+    })
+
+    wx.setStorage({
+      key: 'historyRecord',
+      data: filterArr
+    })
+  },
+  saveHistory (param) {
+    let arr = this.data.historyRecord
+    arr.unshift(param)
+    wx.setStorage({
+      key: 'historyRecord',
+      data: arr
+    })
+    this.setData({
+      historyRecord: arr
+    })
+  }
+})
