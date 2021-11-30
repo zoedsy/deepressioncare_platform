a3a5cd669ef53234dc4b52eeea9179bd6f2234f6
许诺 <494296904@qq.com>
2021-11-30 15:13:55 +0800

    个人

--- pages/me/me.js
+++ pages/me/me.js
@@ -1,12 +1,54 @@
-// pages/me/me.js
+// pages/person/index.js
 Page({
+
+
+
+  data:{
+    userInfo: {},
+    hasUserInfo: false,
+  
+  },
+  
+  getUserProfile(e) {
+    
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
+    // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        // console.log(res),
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+      
+    })
+    // wx.setStorageSync('userinfo', 'userInfo'),
+    
+    // wx.navigateBack({
+    //   delta: 1
+    // });
+  },
 
   /**
    * 页面的初始数据
    */
-  data: {
+  // data: {
+  //   userinfo:{},
+  //   // nickname:'',
+  //   // headUrl:''
+  
+  
 
-  },
+  // },
+  // onShow(){
+  //   const userinfo=wx.getStorageSync("userinfo");
+  //   this.setData({userinfo});
+  //   // nickname=userinfo.nickName,
+  //   // headUrl=userinfo.avatarUrl
+
+  // },
 
   /**
    * 生命周期函数--监听页面加载
