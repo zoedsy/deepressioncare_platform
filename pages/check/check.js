29079642e936c71ad0b47cfa862a91cb6b6c5126
Summer <65903839+Summer-Row@users.noreply.github.com>
2021-11-30 19:38:10 +0800

    Create check.js

---
+++ pages/check/check.js
@@ -0,0 +1,27 @@
+Page({
+  data: {
+    // percent:25 percent:50   percent:75 percent: 100
+    percent: 50
+    // probability:80%,
+
+
+  },
+  Patient:function (name,audio_Type,date) {
+    this.setData({
+      Name:name,
+      Type:audio_Type,
+      Date:date,
+    })
+      
+    },
+  goToeva_result:function(){
+    wx.navigateTo({
+      url: '../eva_result/eva_result',
+    })
+  },
+  onLoad: function (options) {
+    this.Patient('王小翰','音频','2021-11-28 09：35：45')
+  },
+})
+
+
