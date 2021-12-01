8a9ca4d2dc1e9b489eb66bbb56b76fa2d8f83669
chutiankuo <61624046+chutiankuo@users.noreply.github.com>
2021-12-01 18:08:14 +0800

    pages/chat/chat.wxml

--- pages/chat/pages/consult/consult.js
+++
@@ -1,8 +0,0 @@
-// pages/zixun/zixun.js
-Page({
-    //跳转到医生列表
-    goto_doctorlist: function(){ wx.navigateTo({ url: '../doctor_list/doctor_list', }) },
-    goto_question: function(){ wx.navigateTo({ url: '../quickquestion/quickquestion', }) }
-  
-} 
-)
