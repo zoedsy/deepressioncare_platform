bab081b15fc940f502f3572eb1341e602b3f9287
Summer <65903839+Summer-Row@users.noreply.github.com>
2021-12-06 16:45:19 +0800

    Update diagnose.js

--- pages/diagnose/diagnose.js
+++ pages/diagnose/diagnose.js
@@ -4,10 +4,23 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    untreated_abnormalList: [
+      {
+          id: 0,
+          images: "../../images/baby.png",
+          title: "历史记录1",
+          detail: "21113222211vvvvvvvvvcccccccccc1"
+      },
+      {
+          id: 1,
+          images: "../../images/baby.png",
+          title: "历史记录2",
+          detail: "2222"
+      },
+  ],
   },
   Patient:function (name,department,date) {
-    this.setData({
+    this.setData({ 
       Name:name,
       Deparment:department,
       Date:date,
@@ -17,9 +30,32 @@ Page({
     
   //上传音频事件处理
   bindViewTap:function(){
-    wx.navigateTo({
-      url: '/pages/diagnose/audio/audio'
+    // wx.navigateTo({
+    //   url: '/pages/audio/audio'
+    // })
+    wx.chooseMessageFile({
+      count: 1,
+      type:'file',
+      success(res){
+        // tempFilePath可以作为临时路径
+        const tempFilePaths = res.tempFilePaths
+        // console.log('选择',res);
+        wx.showModal({
+          title:'您的文件已上传成功！',
+          content:'点击“确定”即可查看详情',
+          success:function(res){
+            if(res.confirm){  //用户点击确定后
+              wx.navigateTo({
+                url: '../check/check'
+              })
+            }else{  //用户点击取消后
+
+            }
+          }
+        })
+      }
     })
+
   },
 // 跳转到个人页面
   goToPerson:function(){
@@ -27,10 +63,15 @@ Page({
       // 跳转个人页面
       // url: '../Person/person',
       // 测试案例
-      url: '/pages/diagnose/check/check'
+      url: '/pages/check/check'
     })
   },
-  
+  goToDetail:function(){
+    wx.navigateTo({
+      // 跳转到历史记录页面
+      url:'/pages/check/check'
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
