de0e0b2a16f09454b2531df05d01d6d9c2bf6c60
Zoedusy <zoedusy123@gmail.com>
2021-12-06 08:55:44 +0800

    gerenzhongxin

--- pages/me/information/index.js
+++ pages/me/information/index.js
@@ -15,90 +15,15 @@ Page({
     height:'',
     weight:'',
     BMI:'',
-    waist:'',
-
-    relation:'',
-    re:'',
-    relationList:['自己', '父母', '子女', '孙子孙女', '亲戚', 
-                  '爷爷奶奶', '外公外婆', '朋友'],
-    // objectRelation: [
-    //   {
-    //     id: 0,
-    //     name: '自己'
-    //   },
-    //   {
-    //     id: 1,
-    //     name: '父母'
-    //   },
-    //   {
-    //     id: 2,
-    //     name: '子女'
-    //   },
-    //   {
-    //     id: 3,
-    //     name: '孙子孙女'
-    //   },
-    //   {
-    //     id: 4,
-    //     name: '亲戚'
-    //   },
-    //   {
-    //     id: 5,
-    //     name: '爷爷奶奶'
-    //   },
-    //   {
-    //     id: 6,
-    //     name: '外公外婆'
-    //   },
-    //   {
-    //     id: 7,
-    //     name: '朋友'
-    //   },
-    // ],
-    relationIndex:0,
-
-    // heightList:['100cm以下','100cm','101cm','102cm','103cm','104cm','105cm','106cm','107cm','108cm','109cm',
-    //                       '110cm','111cm','112cm','113cm','114cm','115cm','116cm','117cm','118cm','119cm',
-    //                       '120cm','121cm','122cm','123cm','124cm','125cm','126cm','127cm','128cm','129cm',
-    //                       '130cm','131cm','132cm','133cm','134cm','135cm','136cm','137cm','138cm','139cm',
-    //                       '140cm','141cm','142cm','143cm','144cm','145cm','146cm','147cm','148cm','149cm',
-    //                       '150cm','151cm','152cm','153cm','154cm','155cm','156cm','157cm','158cm','159cm',
-    //                       '160cm','161cm','162cm','163cm','164cm','165cm','166cm','167cm','168cm','169cm',
-    //                       '170cm','171cm','172cm','173cm','174cm','175cm','176cm','177cm','178cm','179cm',
-    //                       '180cm','181cm','182cm','183cm','184cm','185cm','186cm','187cm','188cm','189cm',
-    //                       '190cm','191cm','192cm','193cm','194cm','195cm','196cm','197cm','198cm','199cm',
-    //                       '200cm','200cm以上'],
-    // heightIndex:0,
-                          
-
+    illness:'',
     smoke:'',
     isSmoke:['是', '否'],
-    // objectSmoke: [
-    //   {
-    //     id: 0,
-    //     name: '是'
-    //   },
-    //   {
-    //     id: 1,
-    //     name: '否'
-    //   },
-    // ],
     smokeIndex:0,
-
     date: '1900-01-01'
   },
 
 
-  relationInput:function(e){
-    // console.log(e);
-    // this.setData({
-    //   re:e.detail.value
-    // })
-    this.setData({
-      relationIndex:e.detail.value,
-      // re:relationList[relationIndex]
-    })
-  },
+  
 
   nameInput:function(e){
     this.setData({
@@ -143,9 +68,9 @@ Page({
     })
   },
 
-  waistInput:function(e){
+  illnessInput:function(e){
     this.setData({
-      waist:e.detail.value
+      illness:e.detail.value
     })
   },
 
@@ -165,7 +90,7 @@ Page({
 
 
   saveBtnClick:function(){
-    if(this.data.re.length == 0 || this.data.na.length == 0
+    if(this.data.na.length == 0
       || this.data.se.length == 0 || this.data.bir.length == 0){
       this.setData({
         infoMess:'温馨提示：必填项不能为空！',
@@ -174,12 +99,14 @@ Page({
     }else{
       this.setData({
         infoMess:'',
-        relation:this.data.re,
         name:this.data.na,
         sex:this.data.se,
         birthday:this.data.bir
       })
     }
+    wx.navigateBack({
+      delta: 1
+    });
   },
 
   
--- pages/me/me.js
+++ pages/me/me.js
@@ -11,15 +11,25 @@ Page({
   },
   
   getUserProfile(e) {
-    //先login，再getUserProfile
-    wx.login({
+      //登录
+    wx.checkSession({
       success: (res) => {
-        console.log("code",res.code)
-        this.setData({
-          code:res.code
+        console.log("session",res)
+      },
+      fail:(err)=>{
+        console.log("session err",err)
+        //过期重新登陆？
+        wx.login({
+          success: (res) => {
+            console.log("code",res.code)
+            this.setData({
+              code:res.code
+            })
+          }
         })
       }
     })
+    
     wx.getUserProfile({
       desc: '用户登录', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
       success: (file) => {
@@ -28,7 +38,6 @@ Page({
           userInfo: file.userInfo,
           hasUserInfo: true
         })
-        
         // 请求 openid,token
         wx.request({
           url: 'http://106.13.28.21:8081/api/wx/wx_login',
@@ -44,11 +53,10 @@ Page({
             'content-type': 'application/json'
           },
           success:(res)=>{  
-            // 这里请求登录接口这块？
             console.log(res.data)
-            app.globalData.openId = res.data.data.id,
+            app.globalData.openId = res.data.data.openId,
             app.globalData.token = res.data.data.token,
-            wx.setStorageSync('openId', res.data.data.id) // 缓存openid
+            wx.setStorageSync('openId', res.data.data.openId) // 缓存openid
             wx.setStorageSync('token', res.data.data.token) //缓存token
             wx.setStorageSync('userInfo', res.data.data.usefInfo)
           },fail:(err)=>{
@@ -106,31 +114,16 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
-    //session是否过期
-    wx.checkSession({
-      success: (res) => {
-        console.log("session",res)
-        // 本地缓存没过期，就直接登录
-        if(wx.getStorageSync('token')){
-          app.globalData.openid = wx.getStorageSync('openId')
-          app.globalData.token = wx.getStorageSync('token')
-          this.setData({
-            userInfo:wx.getStorageSync('userInfo')
-          })
-        }
-      },
-      fail:(err)=>{
-        console.log("session err",err)
-        //过期重新登陆
-        if(wx.getStorageSync('token')){
-          wx.showToast({
-            title: '用户登录已过期，请重新登录',
-          })
-        }
-      },
-    })
+    // 如果有本地缓存的openid，就直接登录
+    if(wx.getStorageSync('token')){
+      app.globalData.openid = wx.getStorageSync('openId')
+      app.globalData.token = wx.getStorageSync('token')
+      this.setData({
+        userInfo:wx.getStorageSync('userInfo')
+      })
+    }
   },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
