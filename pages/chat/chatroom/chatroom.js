7d3c9c34d137cf80cba2e545746880ac1fd8a7d9
Zoedusy <zoedusy123@gmail.com>
2021-12-06 16:36:44 +0800

    chatroom

---
+++ pages/chat/chatroom/chatroom.js
@@ -0,0 +1,75 @@
+// pages/chat/chatroom/chatroom.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.navigateTo({
+      url: '../../home/doctor/doctor',
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  // 需要实时刷新，所以onshow里面
+  onShow: function () {
+    // 获取医生列表函数执行
+
+  },
+
+  toDengLu(){
+    wx.navigateTo({
+      url: 'pages/me/me',
+    })
+  },
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
